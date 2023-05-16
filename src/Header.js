11338344a78d3e2272cad0444b4f20dee51dabcc
Yuya Hirano <v322sk@gmail.com>
2023-05-16 10:26:38 -0700

    fix

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div>
         <h5>Y.H</h5>
       </div>
-      <div>
+      <div className="menu-area">
         <ul className="menu gnavi">
           <li>
             <a href="#">About</a>
