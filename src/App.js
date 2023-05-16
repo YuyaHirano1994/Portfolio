12401ab948ca778ac3e194f79ffc630b14d52fc4
Yuya Hirano <v322sk@gmail.com>
2023-05-16 13:42:28 -0700

    add aboutme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
 import Header from "./Header";
 import Hero from "./Hero";
 import "./styles/App.css";
+import about_img from "./images/image1.jpeg";
+import { FaCode, FaJava } from "react-icons/fa";
+
+import {
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNodedotjs,
+  SiHtml5,
+  SiCss3,
+  SiSass,
+  SiTailwindcss,
+  SiOracle,
+  SiSupabase,
+  SiMysql,
+  SiGraphql,
+  SiAmazonaws,
+  SiGithub,
+} from "react-icons/si";
 
 function App() {
+  const skills = [
+    { icon: SiJavascript, name: "JavaScript" },
+    { icon: SiTypescript, name: "TypeScript" },
+    { icon: SiReact, name: "React" },
+    { icon: SiNodedotjs, name: "Node.js" },
+    { icon: FaJava, name: "Java" },
+    { icon: SiHtml5, name: "HTML5" },
+    { icon: SiCss3, name: "CSS3" },
+    { icon: SiSass, name: "Sass" },
+    { icon: SiTailwindcss, name: "Tailwind" },
+    { icon: SiSupabase, name: "Supabase" },
+    { icon: SiMysql, name: "MySQL" },
+    { icon: SiOracle, name: "OracleDB" },
+    { icon: SiGraphql, name: "GraphQL" },
+    { icon: SiAmazonaws, name: "AWS" },
+    { icon: SiGithub, name: "Git" },
+    { icon: FaCode, name: "Other" },
+  ];
+
   return (
-    <div className="container">
-      <div className="row">
-        <Header />
-        <Hero />
+    <>
+      <div className="container">
+        <div className="row">
+          <Header />
+          <Hero />
+          <div className="col-lg-8 col-md-12 about">
+            <div className="about-wrapper">
+              <div className="about-title">
+                <div className="title">
+                  <h2>about me</h2>
+                </div>
+              </div>
+              <div className="about-content">
+                <div className="about-image">
+                  <img src={about_img} alt="image1"></img>
+                </div>
+                <div className="about-desc">
+                  <p>Hi, there! I'm, Yuya Hirano.</p>
+                  <p>
+                    A Full-Stack Web Developer passionate about creating interactive applications and experiences on the
+                    web.
+                  </p>
+                  <p>I have experience of web developer and system engineer!</p>
+                </div>
+              </div>
+              <div className="skill-area">
+                <div className="skill-icons">
+                  {skills.map((skill, index) => (
+                    <div className="skill-icon" key={index}>
+                      <skill.icon />
+                      <p>{skill.name}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
