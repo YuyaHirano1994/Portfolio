46fc8d1896d097c44d49200c6be21da13fbf7a6c
Yuya Hirano <v322sk@gmail.com>
2023-05-17 13:40:03 -0700

    refa

--- src/CareerData.js
+++ src/CareerData.js
@@ -2,7 +2,6 @@ export const data = [
   {
     title: "Apr 2016",
     cardTitle: "Started my career",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
     cardSubtitle: "SPIC.inc",
     cardDetailedText:
       "After graduating from Tokyo International University, I joined SPIC, a Japanese IT corporation. While my major in university was in the field of international economics, I had a strong desire to work for an IT company at that time. SPIC, which welcomed me despite my lack of experience, provided me with the opportunity to learn programming and problem-solving methods.",
@@ -10,7 +9,6 @@ export const data = [
   {
     title: "Jun 2016",
     cardTitle: "First Project",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
     cardSubtitle: "SPIC.inc",
     cardDetailedText:
       "Maintained HR systems for a security company, including tasks such as requirement definition, development, and maintenance. Used Oracle PeopleSoft (PeopleCode) and OracleDB, with some VBA development. Gained a basic understanding of development and its role in maintenance.",
@@ -18,7 +16,6 @@ export const data = [
   {
     title: "Aug 2018",
     cardTitle: "Second Project",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
     cardSubtitle: "SPIC.inc",
     cardDetailedText:
       "Implemented a new HR system for a construction company as a programmer, using Oracle PeopleSoft (PeopleCode) and OracleDB, with some VBA development. Improved programming skills.",
@@ -26,7 +23,6 @@ export const data = [
   {
     title: "Apr 2019",
     cardTitle: "Third Project",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
     cardSubtitle: "SPIC.inc",
     cardDetailedText:
       "Implemented a new internal system for a gas company as the contract issuance system lead. Used Java and MySQL and managed a team of 4.",
@@ -34,7 +30,6 @@ export const data = [
   {
     title: "Mar 2021",
     cardTitle: "Fourth Project",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
     cardSubtitle: "SPIC.inc",
     cardDetailedText:
       "Developed a form issuing system for a car parts company using MySQL, as part of the implementation of a new in-house system.",
@@ -42,23 +37,18 @@ export const data = [
   {
     title: "Feb 2022",
     cardTitle: "Move to Canada",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
-    // cardSubtitle: "宮沢賢治、死す",
     cardDetailedText:
       "I have dedicated my career to the development of enterprise systems, but I wanted to try my hand at web application development. With this goal in mind, I decided to enroll at a school in Canada. One of the reasons I chose Canada was because I wanted to test my skills in a country other than Japan and challenge myself with a new experience abroad and in a different cultural setting.",
   },
   {
     title: "Aug 2022",
     cardTitle: "Started studying web development.",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
-    // cardSubtitle: "宮沢賢治、死す",
     cardDetailedText:
       "I had been self-learning web development on my own, and starting from August 2022, I began taking a web application development course at Cornerstone",
   },
   {
     title: "Nov 2022",
     cardTitle: "Started my career as Front-end Developer",
-    // url: "https://ja.wikipedia.org/wiki/%E8%AA%95%E7%94%9F",
     cardSubtitle: "Eventer.inc",
     cardDetailedText:
       "Working as a freelance Front-end developer since November 2022.Involved in the development of event marketing platforms using React, JavaScript, and TypeScript.Initially developed using JavaScript, currently transitioning to TypeScript.Experienced in utilizing GraphQL and AWS in the development process.",
